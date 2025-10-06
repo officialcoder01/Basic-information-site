16ed8104ce113204cd3f122f307c8bc06ee2eed7
Zion Musa <musazion986@gmail.com>
2025-10-06 21:07:50 +0100

    Updated app.js routes to render views with escaped objects (title, message) so data is displayed safely.

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
 
 const PORT = process.env.PORT || 8080;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index', { message: 'Welcome to My Website', title: 'This is the home page.' });
 });
-
+ 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.render('about', { title: 'About Us', message: 'Welcome to our website. We are dedicated to providing the best service possible.' });
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/views/contact-me.html');
+    res.render('contact-me', { title: 'Contact Me', message: 'If you have any questions, feel free to reach out!' });
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(__dirname + '/views/404.html');
+    res.status(404).render('404', { title: '404 - Page Not Found', message: 'Sorry, the page you are looking for does not exist.' });
 });
 
 app.listen(PORT, () => {
